f19327f6db048cb1a420c8e9b15189a33df31cbc
Ganesh <ganeshgo1999@gmail.com>
2021-07-12 14:58:32 +0530

    first commit

---
+++ src/main/java/LearnThreads.java
@@ -0,0 +1,18 @@
+public class LearnThreads extends Thread{
+    public static int amount = 0;
+        public void run() {
+            amount++ ;
+            System.out.println("This code is running in a thread");
+    }
+    public static void main(String[] args) {
+        LearnThreads thread = new LearnThreads();
+        thread.start();
+        while(thread.isAlive()) {
+            System.out.println("Waiting...");
+        }
+        System.out.println(amount);
+        amount++;
+        System.out.println(amount);
+        System.out.println("This code is outside of the thread");
+    }
+}
